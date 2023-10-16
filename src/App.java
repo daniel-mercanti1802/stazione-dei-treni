262cbc715934afcdd60f340b2f109537d23a8a3d
daniel-mercanti1802 <145647966+daniel-mercanti1802@users.noreply.github.com>
2023-10-16 11:25:36 +0200

    ddddddddddd

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Biglietteria b=new Biglietteria (10,20,'c');
+           System.out.println(b.toString());
+
+
+           Cliente c=new Cliente("tony","tazza", 15, "f");
+           System.out.println(c.toString());
+
+            Treni t=new Treni(50,100,55);
+           System.out.println(t.toString());
     }
 }
